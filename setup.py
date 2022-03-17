4c8d0ab35f05fad25aa5951341bd867b94ed0ebb
BMeyn <50145395+BMeyn@users.noreply.github.com>
2022-03-17 21:15:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('requirements.txt') as f:
 if __name__ == '__main__':
     setup(
         name='BMeyn',
-        version=os.getenv('PACKAGE_VERSION', '0.0.dev0'),
+        version=os.getenv('PACKAGE_VERSION'),
         package_dir={'': 'src'},
         packages=find_packages('src', include=[
             'demo*'
